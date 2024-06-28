e12a5b9146d93fdb7b19ff42c2950568b47cbbd2
MohamedMao <mohammad.am000@gmail.com>
2024-06-29 02:25:45 +0300

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart'; // for Icons
-import 'functions.dart';
+import 'classes.dart';
 import 'mqtt/mqtt_manager.dart';
 import 'mqtt/mqtt_state.dart';
 import 'package:provider/provider.dart';
 import 'widgets.dart';
-import 'mqtt/setters.dart';
+import 'models.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,10 +17,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: ChangeNotifierProvider<MQTTAppState>(
-      create: (_) => MQTTAppState(),
-      child: const MyHomePage(title: 'Parking Air Quality'),
-    ));
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider<MQTTAppState>(
+            create: (_) => MQTTAppState(),
+          ),
+          ChangeNotifierProvider<Inputs>(
+            create: (_) => Inputs(),
+          ),
+        ],
+        child: const MyHomePage(title: 'Parking Air Quality'),
+      ),
+    );
   }
 }
 
@@ -60,154 +68,103 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    bool autoOn = fanMode;
-    bool doorOpened = doorval; // depending on server info
-    bool fanOn = fanval; // depending on server info
-    double co = coval; // depending on server info
-    double smoke = smokeval; // depending on server info
+    final Inputs provider = Provider.of<Inputs>(context);
+
+    // takes values from provider
+    bool autoOn = provider.fanMode;
+    bool doorOpened = provider.doorval;
+    bool fanOn = provider.fanval;
+    double co = provider.coval;
+    double smoke = provider.smokeval;
 
     return Scaffold(
-      appBar: appbar(),
-      body: ListView(padding: const EdgeInsets.only(top: 10), children: [
-        Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.fromLTRB(0, 10, 0, 20),
-              color: const Color.fromARGB(255, 238, 255, 251),
-              child: Column(
-                children: [
-                  const Text(
-                    'DOOR CONTROL',
-                    style: TextStyle(
-                      color: Colors.amber,
-                      fontSize: 27,
-                      fontWeight: FontWeight.bold,
+        appBar: appbar(),
+        body: ListView(padding: const EdgeInsets.only(top: 10), children: [
+          Column(
+            children: [
+              Container(
+                // Door Status
+                padding: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+                color: const Color.fromARGB(255, 238, 255, 251),
+                child: Column(
+                  children: [
+                    const Text(
+                      'DOOR STATUS',
+                      style: TextStyle(
+                        color: Colors.amber,
+                        fontSize: 27,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  // Text('$co and $smoke, $fanOn, $doorOpened'),
-                  const SizedBox(height: 15), //مساحة بين العناصر
-                  SwitchT(
-                      // Door Switch
-                      valueChanged: (val) {
+                    const SizedBox(height: 15), //مساحة بين العناصر
+                    StatusCard(doorStatus: doorOpened),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 30), //مساحة بين العناصر
+              const Text(
+                'FAN CONTROL',
+                style: TextStyle(
+                  color: Colors.amber,
+                  fontSize: 27,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SwitchT(
+                // AutoMode Switch
+                boolval: autoOn,
+                txt: 'Automatic Mode',
+                ico: Icon(
+                  FontAwesomeIcons.robot,
+                  size: 40,
+                  color: Colors.grey[500],
+                ),
+                valueChanged: (val) {
+                  setState(() {
+                    autoOn = val;
+                    (autoOn)
+                        ? manager.publish('1', 'control/fan')
+                        : manager.publish('0', 'control/fan');
+                  });
+                },
+                sub: const Text(
+                    'Enabling this option allows sensors to control the fan automatically'),
+              ),
+              SwitchT(
+                // Fan Switch
+                boolval: fanOn,
+                txt: (fanOn) ? 'Opened' : 'Closed',
+                ico: Icon(
+                  FontAwesomeIcons.fan,
+                  size: 40,
+                  color: (fanOn) ? Colors.green[400] : Colors.grey[500],
+                ),
+                valueChanged: autoOn
+                    ? null
+                    : (val) {
                         setState(() {
-                          doorOpened = val;
-                          (doorOpened)
-                              ? manager.publish('open', 'door/status')
-                              : manager.publish('close', 'door/status');
+                          fanOn = val;
+                          (fanOn)
+                              ? manager.publish('1', 'control/fan/manual')
+                              : manager.publish('0', 'control/fan/manual');
                         });
                       },
-                      boolval: doorOpened,
-                      txt: (doorOpened) ? 'Opened' : 'Closed',
-                      ico: Icon(
-                        FontAwesomeIcons.doorOpen,
-                        size: 40,
-                        color:
-                            (doorOpened) ? Colors.green[400] : Colors.grey[500],
-                      )),
-                ],
               ),
-            ),
-            const SizedBox(height: 30), //مساحة بين العناصر
-            const Text(
-              'FAN CONTROL',
-              style: TextStyle(
-                color: Colors.amber,
-                fontSize: 27,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SwitchT(
-              // AutoMode Switch
-              boolval: autoOn,
-              txt: 'Automatic Mode',
-              ico: Icon(
-                FontAwesomeIcons.robot,
-                size: 40,
-                color: Colors.grey[500],
-              ),
-              valueChanged: (val) {
-                setState(() {
-                  autoOn = val;
-                  (autoOn)
-                      ? manager.publish('1', 'control/fan')
-                      : manager.publish('0', 'control/fan');
-                });
-              },
-              sub: const Text(
-                  'Enabling this option allows sensors to control the fan automatically'),
-            ),
-            SwitchT(
-              // Fan Switch
-              boolval: fanOn,
-              txt: (fanOn) ? 'Opened' : 'Closed',
-              ico: Icon(
-                FontAwesomeIcons.fan,
-                size: 40,
-                color: (fanOn) ? Colors.green[400] : Colors.grey[500],
-              ),
-              valueChanged: autoOn
-                  ? null
-                  : (val) {
-                      setState(() {
-                        fanOn = val;
-                        (fanOn)
-                            ? manager.publish('1', 'control/fan/manual')
-                            : manager.publish('0', 'control/fan/manual');
-                      });
-                    },
-            ),
-            const SizedBox(height: 20), //مساحة بين العناصر
-            Container(
-              color: const Color.fromARGB(255, 238, 255, 251),
-              child: Column(
-                children: [
-                  const SizedBox(height: 25), //مساحة بين العناصر
-                  const Text(
-                    'CO',
-                    style: TextStyle(
-                      color: Colors.amber,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SFG(
-                    gas: co,
-                    max: 66,
-                  ), //استدعاء عداد للغاز
-                ],
-              ),
-            ),
+              const SizedBox(height: 20), //مساحة بين العناصر
 
-            Container(
-              color: const Color.fromARGB(255, 231, 255, 250),
-              child: Column(
-                children: [
-                  const SizedBox(height: 25), //مساحة بين العناصر
-                  const Text(
-                    'SMOKE',
-                    style: TextStyle(
-                      color: Colors.amber,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SFG(
-                    gas: smoke,
-                    max: 33,
-                  ), //استدعاء عداد للغاز
-                ],
-              ),
-            )
-          ],
-        )
-      ]),
-    );
+              // Gas Meters
+              SFG(gas: co, max: 66, name: 'CO'),
+              SFG(gas: smoke, max: 33, name: 'SMOKE'),
+            ],
+          )
+        ]));
   }
 
   void _configureAndConnect() {
     manager = MqttManager(state: currentAppState);
-    manager.initializeMQTTClient();
+    manager.initializeMQTTClient(context);
     manager.connect();
+    manager.client!.onConnected = () => manager.onConnected(context);
   }
 
   // void _disconnect() {
--- lib/mqtt/mqtt_manager.dart
+++ lib/mqtt/mqtt_manager.dart
@@ -1,38 +1,49 @@
 // ignore_for_file: avoid_print
 
+import 'package:flutter/material.dart';
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:mqtt_client/mqtt_server_client.dart';
+import 'package:provider/provider.dart';
 import 'mqtt_state.dart';
-import 'setters.dart';
+import '../models.dart';
 
 class MqttManager {
   final MQTTAppState _currentState;
   MqttServerClient? _client;
-  // final String _identifier;
+
+  // MQTT topic names
   final String _coTopic = 'co/level';
   final String _smokeTopic = 'smoke/level';
   final String _fanTopic = 'fan/status';
   final String _doorTopic = 'door/status';
   final String _fanmodeTopic = 'control/fan';
   // final String _manualFanTopic = 'control/fan/manual';
+
+  // server info
   final String host = 'broker.hivemq.com';
   final String clientId = 'MqttDashboard-9925752';
   final String username = 'IOT_Park';
   final String password = 'Parkin1234';
 
+  // client get
+  MqttServerClient? get client => _client;
+
+  // Inputs provider = Inputs();
+  late Inputs _provider;
+
   MqttManager({required MQTTAppState state}) : _currentState = state;
 
-  void initializeMQTTClient() {
+  void initializeMQTTClient(BuildContext context) {
     // _client = MqttServerClient(host, _identifier);
     _client = MqttServerClient.withPort(host, clientId, 1883);
     _client!.port = 1883;
     _client!.keepAlivePeriod = 10;
     _client!.onDisconnected = onDisconnected;
-    // _client!.secure = false;
     _client!.logging(on: false);
+    // _client!.secure = false; // for secure connection
 
-    /// Add the successful connection callback
-    _client!.onConnected = onConnected;
+    // Add the successful connection callback
+    _client!.onConnected = () => onConnected(context);
     _client!.onSubscribed = onSubscribed;
 
     final MqttConnectMessage connMess = MqttConnectMessage()
@@ -44,23 +55,26 @@ class MqttManager {
     _client!.connectionMessage = connMess;
   }
 
+  // connect function
   void connect() async {
     assert(_client != null);
     try {
-      print('EXAMPLE: start client connecting....');
+      print('## start client connecting....');
       _currentState.setAppConnectionState(MQTTAppConnectionState.connecting);
       await _client!.connect();
     } on Exception catch (e) {
-      print('EXAMPLE::client exception - $e');
+      print('## client exception - $e');
       disconnect();
     }
   }
 
+  // disconnect function
   void disconnect() {
     print('Disconnected');
     _client!.disconnect();
   }
 
+  // publish function
   void publish(String message, String topic) {
     final MqttClientPayloadBuilder builder = MqttClientPayloadBuilder();
     builder.addString(message);
@@ -83,9 +97,10 @@ class MqttManager {
   }
 
   /// The successful connect callback
-  void onConnected() async {
+  void onConnected(BuildContext context) async {
     _currentState.setAppConnectionState(MQTTAppConnectionState.connected);
-    print('EXAMPLE:: client connected....');
+    print('## client connected....');
+    _provider = Provider.of<Inputs>(context, listen: false);
 
     // Subscribe to all topics
     _client!.subscribe(_coTopic, MqttQos.atLeastOnce);
@@ -101,24 +116,24 @@ class MqttManager {
           MqttPublishPayload.bytesToStringAsString(payload.payload.message);
       final String topic = c[0].topic;
 
-      // Handle message based on the topic
+      // Send input message to provider based on the topic
       if (topic == _coTopic) {
-        setCO(message);
+        _provider.updateCo(message);
       } else if (topic == _smokeTopic) {
-        setSmoke(message);
+        _provider.updateSmoke(message);
       } else if (topic == _doorTopic) {
-        setDoor(message);
+        _provider.updateDoor(message);
       } else if (topic == _fanTopic) {
-        setFan(message);
-      } else if (topic == _fanmodeTopic) {}
+        _provider.updateFan(message);
+      } else if (topic == _fanmodeTopic) {
+        _provider.updateMode(message);
+      }
 
-      print(
-          'EXAMPLE::Change notification:: topic is <$topic>, payload is <-- $message -->');
+      print('topic is <$topic>, payload is <-- $message -->');
       print('');
     });
 
-    print(
-        'EXAMPLE::OnConnected client callback - Client connection was successful');
+    print('## OnConnected client callback - Client connection was successful');
 
     // _client!.disconnect();
   }
